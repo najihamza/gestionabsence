53c69b3cff9cdd40265061c22bead20578db5275
hamza <najihamza359@gmail.com>
2014-04-26 00:41:18 +0100

    comentaire

--- gestion_d_absence v1/src/com/ihm/home2.java
+++ gestion_d_absence v1/src/com/ihm/home2.java
@@ -1,11 +1,13 @@
 package com.ihm;
-
+//zid hadi alah idiik
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -14,19 +16,28 @@ import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
-public class home2  extends JFrame{
-	ImageIcon one = new ImageIcon("images/one.jpg");
-	ImageIcon tow = new ImageIcon("images/tow.jpg");
-	ImageIcon tree = new ImageIcon("images/tree.jpg");
-	ImageIcon four = new ImageIcon("images/four.jpg");
+public class home2  extends JFrame implements MouseListener{
+	ImageIcon one = new ImageIcon("images/avant.png");
+	ImageIcon one1 = new ImageIcon("images/apres.png");
+	ImageIcon tow = new ImageIcon("images/etu.jpg");
+	ImageIcon tow1 = new ImageIcon("images/etu1.jpg");
+	ImageIcon tree = new ImageIcon("images/stat.jpg");
+	ImageIcon tree1 = new ImageIcon("images/stat1.jpg");
+	ImageIcon four = new ImageIcon("images/set.png");
+	ImageIcon four1 = new ImageIcon("images/set1.png");
 	ImageIcon logos = new ImageIcon("images/logo.png");
 	private JPanel contentPane;
 	
 	public home2() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 
 		Dimension dim = GuiTools.getDimScreen();
 		setSize((dim.width / 2 )+(dim.width / 4)  , (dim.height /2)+(dim.height / 3)+(dim.height / 9));
-//		setResizable(false);
+		
+		
+		
+		
+		setResizable(false);
 	/***************************************************************************/
 
 		contentPane = new JPanel();
@@ -37,7 +48,7 @@ public class home2  extends JFrame{
 		JPanel north = new JPanel();
 		contentPane.add(north, BorderLayout.NORTH);
 		GridBagLayout gbl_north = new GridBagLayout();
-		gbl_north.columnWidths = new int[] {0, 6, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 0, 0};
+		gbl_north.columnWidths = new int[] {0, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 0, 0};
 		gbl_north.rowHeights = new int[]{0, 0};
 		gbl_north.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0};
 		gbl_north.rowWeights = new double[]{0.0, Double.MIN_VALUE};
@@ -45,50 +56,50 @@ public class home2  extends JFrame{
 		
 		JLabel logo = new JLabel(logos);
 		GridBagConstraints gbc_logo = new GridBagConstraints();
-		gbc_logo.anchor = GridBagConstraints.SOUTHEAST;
+ 		gbc_logo.anchor = GridBagConstraints.SOUTHEAST;
 		gbc_logo.insets = new Insets(0, 0, 0, 5);
 		gbc_logo.gridx = 0;
 		gbc_logo.gridy = 0;
-		gbc_logo.gridwidth = 2;
+		gbc_logo.gridwidth = 1;
 		north.add(logo, gbc_logo);
 		
 		JLabel bienvenu = new JLabel("bienvenu");
 		bienvenu.setHorizontalAlignment(SwingConstants.LEFT);
 		GridBagConstraints gbc_bienvenu = new GridBagConstraints();
- 		gbc_bienvenu.anchor = GridBagConstraints.NORTHEAST;
+ 	gbc_bienvenu.anchor = GridBagConstraints.NORTHEAST;
 		gbc_bienvenu.insets = new Insets(0, 0, 0, 5);
 		gbc_bienvenu.gridx = 21;
 		gbc_bienvenu.gridy = 0;
-		gbc_bienvenu.gridwidth = 2;
+		gbc_bienvenu.gridwidth = 1;
 		north.add(bienvenu, gbc_bienvenu);
 		
 		JLabel login = new JLabel("Rachid wazaani");
 		login.setHorizontalAlignment(SwingConstants.LEFT);
 		GridBagConstraints gbc_login = new GridBagConstraints();
-		gbc_login.gridwidth = 2;
- 		gbc_login.anchor = GridBagConstraints.NORTH;
-		gbc_login.gridx = 23;
+		 
+ 		 gbc_login.anchor = GridBagConstraints.NORTH;
+		gbc_login.gridx = 22;
 		gbc_login.gridy = 0;
 		gbc_login.gridwidth = 2;
 		north.add(login, gbc_login);
 		
-		
-		JLabel aaa = new JLabel("..");
-		login.setHorizontalAlignment(SwingConstants.LEFT);
-		GridBagConstraints gbc_logina = new GridBagConstraints();
-		gbc_logina.gridwidth = 5;
-		gbc_logina.anchor = GridBagConstraints.NORTHEAST;
-		gbc_logina.gridx = 28;
-		gbc_logina.gridy = 0;
-		gbc_logina.gridwidth = 2;
-		north.add(aaa, gbc_login);
+//		JLabel logina = new JLabel("...." );
+//		login.setHorizontalAlignment(SwingConstants.LEFT);
+//		GridBagConstraints gbc_logina = new GridBagConstraints();
+//		gbc_login.gridwidth = 2;
+// 	 gbc_login.anchor = GridBagConstraints.NORTH;
+//		gbc_login.gridx = 22;
+//		gbc_login.gridy = 0;
+//		gbc_login.gridwidth = 1;
+//		north.add(logina, gbc_login);
+	 
 		
 		
 		JPanel center = new JPanel();
 		contentPane.add(center, BorderLayout.CENTER);
 		GridBagLayout gbl_center = new GridBagLayout();
-		gbl_center.columnWidths = new int[] {6, 6, 30, 30, 30, 30, 30, 0, 0, 0};
-		gbl_center.rowHeights = new int[] {6, 6, 30, 30, 30, 30, 0, 0, 0};
+		gbl_center.columnWidths = new int[] {30, 30, 30, 30, 30, 30, 30, 0, 0, 0};
+		gbl_center.rowHeights = new int[] {30, 30, 30, 30, 30, 30, 0, 0, 0};
 		gbl_center.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		gbl_center.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		 
@@ -99,8 +110,8 @@ public class home2  extends JFrame{
 		gbc_gestion_etudiant.insets = new Insets(0, 0, 5, 5);
 		gbc_gestion_etudiant.gridx = 7;
 		gbc_gestion_etudiant.gridy = 6;
-		gbc_gestion_etudiant.gridwidth = 2;
-		gbc_gestion_etudiant.fill = GridBagConstraints.BOTH;
+		gbc_gestion_etudiant.gridwidth = 1;
+//		gbc_gestion_etudiant.fill = GridBagConstraints.BOTH;
 		center.add(gestion_etudiant, gbc_gestion_etudiant);
 		
 		JLabel lblGestionDabsence = new JLabel(tow);
@@ -108,8 +119,8 @@ public class home2  extends JFrame{
 		gbc_lblGestionDabsence.insets = new Insets(0, 0, 5, 0);
 		gbc_lblGestionDabsence.gridx = 8;
 		gbc_lblGestionDabsence.gridy = 6;
-		gbc_lblGestionDabsence.gridwidth = 2;
-		gbc_lblGestionDabsence.fill = GridBagConstraints.BOTH;
+		gbc_lblGestionDabsence.gridwidth = 1;
+//		 gbc_lblGestionDabsence.fill = GridBagConstraints.BOTH;
 		center.add(lblGestionDabsence, gbc_lblGestionDabsence);
 		
 		JLabel lblStatistiques = new JLabel(tree);
@@ -117,8 +128,8 @@ public class home2  extends JFrame{
 		gbc_lblStatistiques.insets = new Insets(0, 0, 0, 5);
 		gbc_lblStatistiques.gridx = 7;
 		gbc_lblStatistiques.gridy = 7;
-		gbc_lblStatistiques.gridwidth =2;
-		gbc_lblStatistiques.fill = GridBagConstraints.BOTH;
+		gbc_lblStatistiques.gridwidth =1;
+//		gbc_lblStatistiques.fill = GridBagConstraints.BOTH;
 		
 		center.add(lblStatistiques, gbc_lblStatistiques);
 		
@@ -126,16 +137,17 @@ public class home2  extends JFrame{
 		GridBagConstraints gbc_lblGestionDeCompte = new GridBagConstraints();
 		gbc_lblGestionDeCompte.gridx = 8;
 		gbc_lblGestionDeCompte.gridy = 7;
-		gbc_lblGestionDeCompte.gridwidth = 2;
-		gbc_lblGestionDeCompte.fill = GridBagConstraints.BOTH;
+		gbc_lblGestionDeCompte.gridwidth = 1;
+//		gbc_lblGestionDeCompte.fill = GridBagConstraints.BOTH;
 		center.add(lblGestionDeCompte, gbc_lblGestionDeCompte);
 		
 		JPanel south = new JPanel();
 		contentPane.add(south, BorderLayout.SOUTH);
-	 
+		JLabel vide = new JLabel( );
+		south.add(vide);
 		JLabel lblEcoleNationaleDes = new JLabel("Ecole Nationale des Sciences Appliqu\u00E9es El Hoceima");
 		south.add(lblEcoleNationaleDes);
- 	 
+		 
 	
 	
 	}
@@ -150,6 +162,31 @@ public class home2  extends JFrame{
 				}
 			}
 		});
+	}
+	 
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	 
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	 
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	 
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	 
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	
